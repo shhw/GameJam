e10915b95882ca0005521bf93f8d13e741273d16
maxi <maxi12896@live.com>
2019-02-23 13:51:03 +0800

    EarthEntity添加

--- WanderingEarth/Assets/Scripts/Entity/EarthEntity.cs
+++ WanderingEarth/Assets/Scripts/Entity/EarthEntity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace WanderingEarth
 {
@@ -11,5 +12,53 @@ namespace WanderingEarth
     /// </summary>
     class EarthEntity : BaseEntity
     {
+        public float InitForce = 1.0f;
+
+        public float SideForce = 1.0f;
+
+        public Vector2 SideDirection;
+
+        Rigidbody2D thisRb2D;
+
+
+        void Awake()
+        {
+            thisRb2D = GetComponent<Rigidbody2D>();
+            thisRb2D.AddForce(Vector2.up * InitForce);
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            Vector2 sideDir = SideDirection - Vector2.right;
+            sideDir = new Vector2(this.transform.right.x, this.transform.right.y) + sideDir;
+            if (Input.GetKey(KeyCode.A))
+            {
+                thisRb2D.AddForce(sideDir * new Vector2(-1, 1) * SideForce);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                thisRb2D.AddForce(sideDir * 1 * SideForce);
+            }
+        }
+
+        public Vector2 GetEarthPosition()
+        {
+            return transform.position;
+        }
+
+        public float GetEarthMass()
+        {
+            return thisRb2D.mass;
+        }
+
+        public void OnAddAttractionForce(Vector2 force)
+        {
+            thisRb2D.AddForce(force);
+        }
     }
 }
