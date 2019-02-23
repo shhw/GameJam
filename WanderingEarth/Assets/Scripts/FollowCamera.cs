9c29a51f70201de66d1509c57af5cba26fa3e5b5
maxi <maxi12896@live.com>
2019-02-23 20:48:50 +0800

    修正地球速度过快问题

--- WanderingEarth/Assets/Scripts/FollowCamera.cs
+++ WanderingEarth/Assets/Scripts/FollowCamera.cs
@@ -6,20 +6,20 @@ public class FollowCamera : MonoBehaviour
 {
     public float LerpSpeed = 0.5f;//几秒
 
-    Transform earthT;
     Vector3 DeltaPos;
 
     // Use this for initialization
     void Start()
     {
-        earthT = WanderingEarth.SceneManager.GetInstance().GetEarthEntity().transform;
-        DeltaPos = earthT.position - transform.position;
+        Vector2 OrgPos = WanderingEarth.SceneManager.GetInstance().GetEarthEntity().GetPosition();
+        DeltaPos = new Vector3(OrgPos.x, OrgPos.y, 0) - transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 LerpPos = earthT.position - DeltaPos;
+        Vector2 OrgPos = WanderingEarth.SceneManager.GetInstance().GetEarthEntity().GetPosition();
+        Vector3 LerpPos = new Vector3(OrgPos.x, OrgPos.y, 0) - DeltaPos;
         transform.position = Vector3.Lerp(transform.position, LerpPos, Time.deltaTime / LerpSpeed);
     }
 }
