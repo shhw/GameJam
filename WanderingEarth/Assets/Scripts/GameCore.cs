ddcaaad8184205407741d9697ee1bd3e40edb2bf
liguochang1993 <liguochang1993@gmail.com>
2019-02-23 13:38:59 +0800

    UIManager方法‘主场景更新

--- WanderingEarth/Assets/Scripts/GameCore.cs
+++ WanderingEarth/Assets/Scripts/GameCore.cs
@@ -21,6 +21,17 @@ namespace WanderingEarth
         void Start()
         {
             // Init Manager
+            EventManager.GetInstance().Init();
+            UIManager.GetInstance().Init();
+            SceneManager.GetInstance().Init();
+            PlanetManager.GetInstance().Init();
+            ControlManager.GetInstance().Init();
+            EnemyManager.GetInstance().Init();
+            EffectManager.GetInstance().Init();
+            AudioManager.GetInstance().Init();
+            ItemManager.GetInstance().Init();
+
+            UIManager.GetInstance().Show("Page_MainMenu");
         }
 
         void Update()
@@ -31,6 +42,15 @@ namespace WanderingEarth
         private void OnDestroy()
         {
             // 销毁
+            EventManager.GetInstance().Final();
+            UIManager.GetInstance().Final();
+            SceneManager.GetInstance().Final();
+            PlanetManager.GetInstance().Final();
+            ControlManager.GetInstance().Final();
+            EnemyManager.GetInstance().Final();
+            EffectManager.GetInstance().Final();
+            AudioManager.GetInstance().Final();
+            ItemManager.GetInstance().Final();
         }
     }
 }
--- WanderingEarth/Assets/Scripts/Manager/UIManager.cs
+++ WanderingEarth/Assets/Scripts/Manager/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace WanderingEarth
 {
@@ -11,12 +12,47 @@ namespace WanderingEarth
     /// </summary>
     class UIManager : BaseManager<UIManager>
     {
+        private GameObject m_Root = null;
+        private GameObject m_mainUI = null;
+
         public override void Init()
         {
+            GameObject uiGobj = GameObject.Find("UI");
+            if (uiGobj != null)
+            {
+                m_Root = uiGobj;
+            }
         }
 
         public override void Final()
         {
+            if (m_mainUI != null)
+            {
+                m_mainUI.SetActive(false);
+                m_mainUI = null;
+            }
+        }
+
+        public void Show(string dlgName, object param = null)
+        {
+            if (m_mainUI != null)
+            {
+                m_mainUI.SetActive(false);
+            }
+
+            m_mainUI = Global.GetItem(m_Root, dlgName);
+            m_mainUI.SetActive(true);
+        }
+
+        public void Close(string dlgName)
+        {
+            if (m_mainUI != null)
+            {
+                m_mainUI.SetActive(false);
+            }
+
+            m_mainUI = Global.GetItem(m_Root, dlgName);
+            m_mainUI.SetActive(false);
         }
     }
 }
