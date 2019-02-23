86719afff3d3b9ae6e0dd5262d08072eeb794a5c
shhw <shhw1991@gmail.com>
2019-02-23 21:49:40 +0800

    planet

--- WanderingEarth/Assets/Scripts/Manager/EnemyManager.cs
+++ WanderingEarth/Assets/Scripts/Manager/EnemyManager.cs
@@ -10,18 +10,24 @@ namespace WanderingEarth
     /// <summary>
     /// 敌人 Manager。
     /// </summary>
-    class EnemyManager : BaseManager<EffectManager>
+    class EnemyManager : BaseManager<EnemyManager>
     {
         public List<GameObject> enemys;
         public override void Init()
         {
+            enemys = new List<GameObject>();
             GameObject obj = Resources.Load<GameObject>("Prefabs/enemy/1");
             GameObject enemy = Instantiate(obj);
+            GameObject enemyNodes = GameObject.Find("EnemyNodes");
+            enemy.transform.parent = enemyNodes.transform;
+            enemy.transform.position = new Vector3(50, 50, 0);
             enemys.Add(enemy);
         }
 
         public override void Final()
         {
+            foreach (GameObject enemy in enemys)
+                GameObject.Destroy(enemy);
         }
     }
 }
