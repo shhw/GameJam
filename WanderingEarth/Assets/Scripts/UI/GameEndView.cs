5d0b305dbc474bdbb34732adf08adb8761c75544
wencong <1729773154@qq.com>
2019-02-23 19:48:49 +0800

    UI

--- WanderingEarth/Assets/Scripts/UI/GameEndView.cs
+++ WanderingEarth/Assets/Scripts/UI/GameEndView.cs
@@ -20,6 +20,7 @@ namespace WanderingEarth
         void OnClick(UnityEngine.GameObject obj)
         {
             UIManager.GetInstance().Show("Page_GameHUD");
+            SceneManager.GetInstance().InitScene();
         }
     }
 }
