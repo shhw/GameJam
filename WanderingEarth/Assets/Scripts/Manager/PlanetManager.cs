f548a3938ae92e27c8a80afde6fb78d3e4c48df0
maxi <maxi12896@live.com>
2019-02-24 14:51:04 +0800

    merge

--- WanderingEarth/Assets/Scripts/Manager/PlanetManager.cs
+++ WanderingEarth/Assets/Scripts/Manager/PlanetManager.cs
@@ -14,7 +14,7 @@ namespace WanderingEarth
     {
         public List<GameObject> planets;
         public List<GameObject> planetsPool;
-        public int planetCount=9;
+        public int planetCount = 9;
         public float maxMass = 20;
         public float minMass = 5;
         public float maxRho = 0.1f;
@@ -24,17 +24,16 @@ namespace WanderingEarth
         {
             planets = new List<GameObject>();
             planetsPool = new List<GameObject>();
-            GameObject planetNode=GameObject.Find("PlanetNodes"); 
-            for (int i=0;i<planetCount;++i)
+            GameObject planetNode = GameObject.Find("PlanetNodes");
+            for (int i = 0; i < planetCount; ++i)
             {
-                string objPath = "Prefabs/planet/" + (i+1).ToString();
+                string objPath = "Prefabs/planet/" + (i + 1).ToString();
                 GameObject orgPlanet = Resources.Load<GameObject>(objPath);
                 GameObject planet = Instantiate(orgPlanet);
                 planet.transform.parent = planetNode.transform;
                 planet.SetActive(false);
                 planetsPool.Add(planet);
             }
-            ShowPlanet(new Vector2(20, 20));
         }
 
         public override void Final()
@@ -64,30 +63,31 @@ namespace WanderingEarth
             int index = UnityEngine.Random.Range(0, length);
             GameObject planet = planetsPool[index];
             planetsPool.RemoveAt(index);
-            planet.SetActive(true);
             planet.transform.position = new Vector3(pos.x, pos.y, 0);
-            float mass= UnityEngine.Random.Range(minMass, maxMass);
-            float rho= UnityEngine.Random.Range(minRho, maxRho);
-            int picture= UnityEngine.Random.Range(1, 11);
+            float mass = UnityEngine.Random.Range(minMass, maxMass);
+            float rho = UnityEngine.Random.Range(minRho, maxRho);
+            int picture = UnityEngine.Random.Range(1, 11);
             string picturePath = "Picture/planet/" + picture.ToString();
             float scale = rho * mass;
             planet.transform.localScale = new Vector3(scale, scale, scale);
             planet.GetComponent<Rigidbody2D>().mass = mass;
             planets.Add(planet);
+
+            planet.SetActive(true);
         }
 
         public void HidePlanet(GameObject planet)
         {
-            for(int i=0;i<planets.Count();++i)
+            for (int i = 0; i < planets.Count(); ++i)
             {
-                if(planets[i]==planet)
+                if (planets[i] == planet)
                 {
                     planet.SetActive(false);
                     planets.RemoveAt(i);
+                    planetsPool.Add(planet);
                     break;
                 }
             }
-            planetsPool.Add(planet);
         }
     }
 }
