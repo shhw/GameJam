2cd0431a0e56d537c6d8cfbb569475499e7760b7
shhw <shhw1991@gmail.com>
2019-02-24 10:52:55 +0800

    matero

--- WanderingEarth/Assets/Scripts/Entity/ItemEntity.cs
+++ WanderingEarth/Assets/Scripts/Entity/ItemEntity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace WanderingEarth
 {
@@ -11,5 +12,10 @@ namespace WanderingEarth
     /// </summary>
     class ItemEntity : BaseEntity
     {
+        public float speed = 10;
+        void Start()
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+        }
     }
 }
--- WanderingEarth/Assets/Scripts/Manager/ItemManager.cs
+++ WanderingEarth/Assets/Scripts/Manager/ItemManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace WanderingEarth
 {
@@ -11,12 +12,57 @@ namespace WanderingEarth
     /// </summary>
     class ItemManager : BaseManager<ItemManager>
     {
+        public List<GameObject> meteorolites;
+        public List<GameObject> meteorolitesPool;
+
         public override void Init()
         {
+            meteorolites = new List<GameObject>();
+            meteorolitesPool = new List<GameObject>();
+            GameObject obj = Resources.Load<GameObject>("Prefabs/meteorolites/1");
+            GameObject meteorolite = Instantiate(obj);
+            GameObject meteoroliteNode = GameObject.Find("ItemNodes");
+            meteorolite.transform.parent = meteoroliteNode.transform;
+            meteorolite.SetActive(false);
+            meteorolitesPool.Add(meteorolite);
+
+            obj = Resources.Load<GameObject>("Prefabs/meteorolites/2");
+            meteorolite = Instantiate(obj);
+            meteorolite.transform.parent = meteoroliteNode.transform;
+            //meteorolite.SetActive(false);
+            meteorolite.transform.position = new Vector3(100, 50, 0);
+            meteorolitesPool.Add(meteorolite);
         }
 
         public override void Final()
         {
+            foreach (GameObject meteorolite in meteorolites)
+                GameObject.Destroy(meteorolite);
+        }
+
+        public void ShowMeteorolite(Vector2 pos)
+        {
+            int length = meteorolitesPool.Count();
+            int index = UnityEngine.Random.Range(0, length);
+            GameObject meteorolite = meteorolitesPool[index];
+            meteorolitesPool.RemoveAt(index);
+            meteorolite.SetActive(true);
+            meteorolite.transform.position = new Vector3(pos.x, pos.y, 0);
+            meteorolites.Add(meteorolite);
+        }
+
+        public void HidePlanet(GameObject meteorolite)
+        {
+            for (int i = 0; i < meteorolites.Count(); ++i)
+            {
+                if (meteorolites[i] == meteorolite)
+                {
+                    meteorolite.SetActive(false);
+                    meteorolites.RemoveAt(i);
+                    break;
+                }
+            }
+            meteorolitesPool.Add(meteorolite);
         }
     }
 }
