75bea4c6dc439ad09098d46be6f6303f240077b7
wencong <1729773154@qq.com>
2019-02-24 16:30:54 +0800

    进度条

--- WanderingEarth/Assets/Scripts/UI/GameHUDView.cs
+++ WanderingEarth/Assets/Scripts/UI/GameHUDView.cs
@@ -15,15 +15,26 @@ namespace WanderingEarth
         Color orange = new Color(1.0f, 0.27f, 0.0f, 1.0f);
         const float limit = 0.1f;
         int times = 0;
+        GameObject fill = null;
+        GameObject fill2 = null;
+        UnityEngine.UI.Slider slider = null;
+        UnityEngine.UI.Slider slider2 = null;
         protected override void Update()
         {
             var textEditor = gameObject.GetComponentInChildren<UnityEngine.UI.Text>();
             textEditor.text = "光年:" + (SceneManager.GetInstance().GetTravelDistance() / 9.460e5).ToString("F5");
-
-            var slider = gameObject.GetComponentInChildren<UnityEngine.UI.Slider>();
+            if (slider==null)
+            {
+                slider = GameObject.Find("Slider").GetComponent<UnityEngine.UI.Slider>();
+            }
+            
             slider.value = SceneManager.GetInstance().GetEarthEntity().GetCurEnergyRate();
 
-            var fill = GameObject.Find("SliderFill");
+            if (fill ==null)
+            {
+                fill = GameObject.Find("SliderFill");
+            }
+            
             Color color = Color.Lerp(Color.red, Color.green, slider.value);
 
             var background = GameObject.Find("SliderBackground");
@@ -37,6 +48,26 @@ namespace WanderingEarth
             }
             times += 1;
             fill.GetComponent<UnityEngine.UI.Image>().color = color;
+
+            if (fill2 ==null)
+            {
+                fill2 = GameObject.Find("DefenderFill");
+            }
+            var e =SceneManager.GetInstance().GetEarthEntity().GetSheilCoolDownRate();
+
+            if (slider2==null)
+            {
+                slider2= GameObject.Find("DefenderSlider").GetComponent<UnityEngine.UI.Slider>();
+            }
+            slider2.value = e;
+            if (e==1.0f)
+            {
+                fill2.GetComponent<UnityEngine.UI.Image>().color = Color.cyan;
+            }
+            else
+            {
+                fill2.GetComponent<UnityEngine.UI.Image>().color = orange;
+            }
         }
     }
 }
