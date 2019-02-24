7820b7e8176eb09dce2255cd98c8db4e8c4da1cd
liguochang1993 <liguochang1993@gmail.com>
2019-02-24 15:28:36 +0800

    no message

--- WanderingEarth/Assets/Scripts/Manager/AudioManager.cs
+++ WanderingEarth/Assets/Scripts/Manager/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace WanderingEarth
 {
@@ -11,8 +12,41 @@ namespace WanderingEarth
     /// </summary>
     class AudioManager : BaseManager<AudioManager>
     {
+        public enum SoundGroupType
+        {
+            None = -1,
+            Sound = 1,        // 背景音
+            Music = 0,   // 音乐
+        }
+
+        private Dictionary<string, AudioClip> audioClipDic = new Dictionary<string, AudioClip>();
+        private List<AudioClip> playingAudio = new List<AudioClip>();
+
+        private AudioSource musicSource;
+        private AudioSource soundSource;
+
         public override void Init()
         {
+            musicSource = GameObject.Find("AudioMusic").GetComponent<AudioSource>();
+            soundSource = GameObject.Find("AudioSound").GetComponent<AudioSource>();
+
+            audioClipDic.Add("bg", Resources.Load<AudioClip>("Audio/bg"));
+            audioClipDic.Add("boom", Resources.Load<AudioClip>("Audio/boom"));
+            audioClipDic.Add("engine", Resources.Load<AudioClip>("Audio/engine"));
+            audioClipDic.Add("fire", Resources.Load<AudioClip>("Audio/fire"));
+            audioClipDic.Add("getProtect", Resources.Load<AudioClip>("Audio/getProtect"));
+            audioClipDic.Add("protect", Resources.Load<AudioClip>("Audio/protect"));
+            audioClipDic.Add("protect1", Resources.Load<AudioClip>("Audio/protect1"));
+            audioClipDic.Add("protect2", Resources.Load<AudioClip>("Audio/protect2"));
+            audioClipDic.Add("protect3", Resources.Load<AudioClip>("Audio/protect3"));
+            audioClipDic.Add("speed", Resources.Load<AudioClip>("Audio/speed"));
+        }
+
+        public void Play(string name)
+        {
+            AudioClip audio;
+            audioClipDic.TryGetValue(name, out audio);
+            soundSource.PlayOneShot(audio);
         }
 
         public override void Final()
